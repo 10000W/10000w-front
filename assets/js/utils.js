1db8ec9ffb89410b211f44494d3b82e511f522c9
Daniil Vasil'ev <dv@idaproject.com>
2021-11-19 01:50:17 +0300

    refactor: удалил utils.js

--- assets/js/utils.js
+++
@@ -1,8 +0,0 @@
-export const getOffset = (el) => {
-  const rect = el.getBoundingClientRect();
-
-  return {
-    top: rect.top + window.pageYOffset,
-    left: rect.left + window.pageXOffset,
-  };
-}
